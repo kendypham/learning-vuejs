ea4c4b18913f78c18f4fa4e0c56a03cc6bcb8b35
Bao Ha <bao.ha@dynagility.com>
2019-10-04 17:09:20 +0700

    first binding data

---
+++ test/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos='todos'/>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo one',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Todo two',
+          completed: true,
+        },
+        {
+          id: 3,
+          title: 'Todo three',
+          completed: false,
+        },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
