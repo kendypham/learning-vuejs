99cdbd371e44ea961dc11fdc514b89598afbbed3
Truong Pham <kendypham050197@gmail.com>
2019-10-07 17:20:10 +0700

    finish to app vue

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,40 +1,43 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos='todos'/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos.vue";
-
+import Header from  './components/layout/Header';
 export default {
   name: "app",
   components: {
-    Todos
+    Header
   },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo one',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Todo two',
-          completed: true,
-        },
-        {
-          id: 3,
-          title: 'Todo three',
-          completed: false,
-        },
-      ]
-    };
-  }
+  
 };
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
--- test/src/main.js
+++ test/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPencilAlt, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
