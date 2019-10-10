5371885b7d101306eb74d52ba6751bdb0cebc6ca
Truong Pham <kendypham050197@gmail.com>
2019-10-10 18:09:20 +0700

    learning

--- basic/src/App.vue
+++ basic/src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Login with Facebook</h1>
+    <facebook-login class="button"
+      appId="2591659420877374"
+      @login="onLogin"
+      @logout="onLogout"
+      @sdk-loaded="sdkLoaded">
+    </facebook-login>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import Vue from 'vue'
+import facebookLogin from 'facebook-login-vuejs';
+ 
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: { facebookLogin},
+  data(){
+    return{
+      isConnected: false,
+      name: '',
+      email: '',
+      personalID: '',
+      FB: undefined
+    }
+  },
+  methods: { 
+  getUserData() {
+      console.log('FB', this.FB);
+      this.FB.api('/me', 'GET', { fields: 'id,name,email' },
+        userInformation => {
+          console.warn("data api",userInformation)
+          this.personalID = userInformation.id;
+          this.email = userInformation.email;
+          this.name = userInformation.name;
+        }
+      )
+    },
+  sdkLoaded(payload) {
+      this.isConnected = payload.isConnected
+      this.FB = payload.FB
+      if (this.isConnected) this.getUserData()
+    },
+  onLogin() {
+      this.isConnected = true
+      this.getUserData()
+    },
+  onLogout() {
+      this.isConnected = false;
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.footer {
+  position: absolute;
+  background: #ccc;
+  margin: 0;
+  bottom: 0;
+  width: 100%;
+}
+body {
+  margin: 0;
 }
 </style>
